ef85c54f9a7000085d8c0e453e82ad4ca533fde6
arianabrizuela <abrizuela2004@gmail.com>
2024-08-31 01:41:13 -0600

    Añadir mapa dinamico a vista

--- src/templates/comunidad.php
+++ src/templates/comunidad.php
@@ -27,6 +27,22 @@ if (isset($_POST['logout'])) {
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
     crossorigin="anonymous"></script>
 
+  <!-- Leaflet CSS / JS-->
+  <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+  <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+
+  <style>
+    #map {
+      height: 600px;
+      width: 100%;
+    }
+  </style>
+
+  <style>
+    .card-text {
+      text-align: justify;
+    }
+  </style>
   <title>Recycle Hub</title>
 </head>
 
@@ -113,40 +129,65 @@ if (isset($_POST['logout'])) {
   </nav>
 
   <!-- Desarrollo -->
+  <!-- Información sobre programas comunitarios -->
   <main class="container my-5">
     <h1 class="text-center mb-4">Programas Comunitarios de Reciclaje en Costa Rica</h1>
-
-    <!-- Información sobre programas comunitarios -->
-    <section class="mb-5">
-      <h2>Programas Comunitarios</h2>
-      <p>
-        En Costa Rica, existen varios programas comunitarios que promueven el reciclaje y la sostenibilidad ambiental. Estos programas están diseñados para educar a la comunidad sobre la importancia del reciclaje y proporcionar recursos para facilitar el proceso.
-      </p>
-      <ul>
-        <li><strong>AmbientaDOS:</strong> AmbientaDOS es el programa de reciclaje y responsabilidad social que promueve una cultura del reciclaje en Costa Rica, declarado de interés nacional. </li>
-        <li><strong>Paisajes sin Plástico:</strong> Iniciativa que promueve el fortalecimiento de la economía circular y la generación de empleos formales en las municipalidades, organizaciones comunitarias, asociaciones de desarrollo, cooperativas y pequeños y medianos emprendimientos dedicados a la recuperación, valorización y reciclaje de los residuos plásticos.</li>
-        <li><strong>Yo hago el cambio:</strong> Promueven el cambio y la concientización socio-ambiental en el Hoteles, Restaurantes y empresas en Costa Rica por medio de la ejecución de prácticas sostenibles, responsables y el servicio de reciclaje integral.</li>
-      </ul>
-    </section>
+    <br>
 
     <section class="mb-5">
-      <h2>Programas Comunitarios</h2>
-      <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div class="col">
+          <div class="card h-100">
+            <br>
+            <img src="../assets/images/AmbientaDOS.jpg" class="card-img-top" alt="AmbientaDOS">
+            <div class="card-body">
+              <h5 class="card-title">AmbientaDOS</h5>
+              <br>
+              <p class="card-text">AmbientaDOS es el programa de reciclaje y responsabilidad social que promueve una cultura del reciclaje en Costa Rica, declarado de interés nacional.</p>
+            </div>
+            <div class="card-body">
+              <a href="https://www.facebook.com/AmbientaDos/?locale=es_LA" class="card-link">AmbientaDOS</a>
+            </div>
+          </div>
         </div>
-        <div class="card-body">
-          <a href="#" class="card-link">Card link</a>
+        <div class="col">
+          <div class="card h-100">
+            <br>
+            <img src="../assets/images/Paisajes sin plastico.png" class="card-img-top" alt="Paisajes sin Plástico">
+            <div class="card-body">
+              <h5 class="card-title">Paisajes sin Plástico</h5>
+              <br>
+              <p class="card-text">Iniciativa que promueve el fortalecimiento de la economía circular y la generación de empleos formales en las municipalidades, organizaciones comunitarias, asociaciones de desarrollo, cooperativas y pequeños y medianos emprendimientos dedicados a la recuperación, valorización y reciclaje de los residuos plásticos.</p>
+            </div>
+            <div class="card-body">
+              <a href="https://paisajesinplastico.cr/" class="card-link">Paisajes sin Plástico</a>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <br>
+            <img src="../assets/images/Yo hago el cambio.png" class="card-img-top" alt="Yo hago el cambio">
+            <div class="card-body">
+              <h5 class="card-title">Yo hago el cambio</h5>
+              <br>
+              <p class="card-text">Promueven el cambio y la concientización socio-ambiental en Hoteles, Restaurantes y empresas en Costa Rica por medio de la ejecución de prácticas sostenibles, responsables y el servicio de reciclaje integral.</p>
+            </div>
+            <div class="card-body">
+              <a href="https://www.yohagoelcambio.org/es-es/" class="card-link">Yo hago el cambio</a>
+            </div>
+          </div>
         </div>
       </div>
     </section>
+    <br>
 
     <!-- Mapa de centros de reciclaje -->
+    <br>
     <section class="mb-5">
-      <h2>Centros de Reciclaje</h2>
-      <div id="map" style="height: 400px;"></div>
+      <h2>Mapa de Centros de Reciclaje</h2>
+      <br>
+      <div id="map"></div>
     </section>
 
     <!-- Horarios de recolección -->
@@ -166,6 +207,78 @@ if (isset($_POST['logout'])) {
     <p>&copy; 2024 Recycle Hub. Todos los derechos reservados.</p>
   </footer>
 
+  <!-- Leaflet Map Script -->
+  <script>
+    var map = L.map('map').setView([9.7489, -83.7534], 9); // Coordenadas de Costa Rica
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    var marker = L.marker([9.9185, -84.0348]).addTo(map)
+      .bindPopup('Reciclaje Ipp')
+      .openPopup();
+
+    var marker = L.marker([9.955725903055319, -84.03145996294812]).addTo(map)
+      .bindPopup('Recicladora Química S.A')
+      .openPopup();
+
+    var marker = L.marker([9.967587867250804, -84.21364098750287]).addTo(map)
+      .bindPopup('Recicladora y Maquila HyO')
+      .openPopup();
+
+    var marker = L.marker([9.9680607353773, -84.22420620880756]).addTo(map)
+      .bindPopup('Centro de Acopio San Rafael')
+      .openPopup();
+
+    var marker = L.marker([10.001158476370389, -84.16447579030088]).addTo(map)
+      .bindPopup('Planta Reciclaje, Florida Bebidas')
+      .openPopup();
+
+    var marker = L.marker([9.985284290982898, -84.15107577965132]).addTo(map)
+      .bindPopup('Recicla Costa Rica MN S.A.')
+      .openPopup();
+
+    var marker = L.marker([10.011840761810273, -84.10242320374859]).addTo(map)
+      .bindPopup('Centro de acopio y reciclaje')
+      .openPopup();
+
+    var marker = L.marker([9.978348620664608, -84.0856045748214]).addTo(map)
+      .bindPopup('Recicladora Eco-Logistica CR')
+      .openPopup();
+
+    var marker = L.marker([9.952565233196, -84.07830896600805]).addTo(map)
+      .bindPopup('Centro de Acopio Tibás Recicla')
+      .openPopup();
+
+    var marker = L.marker([9.952987928079681, -84.05307474308684]).addTo(map)
+      .bindPopup('Centro de recepcion de reciclaje PROGEA de Costa Rica')
+      .openPopup();
+
+    var marker = L.marker([9.95241330486598, -84.02913635632486]).addTo(map)
+      .bindPopup('CCM Centro de Clasificación de Materiales')
+      .openPopup();
+
+    var marker = L.marker([9.984341846520778, -83.98443390832838]).addTo(map)
+      .bindPopup('Reciclaje Alfaro')
+      .openPopup();
+
+    var marker = L.marker([9.920556522453488, -84.10459432071508]).addTo(map)
+      .bindPopup('Centro de Reciclaje Municipalidad de San Jose')
+      .openPopup();
+
+    var marker = L.marker([9.982735156524399, -84.22532664272556]).addTo(map)
+      .bindPopup('Reciclaje Valenciano')
+      .openPopup();
+
+    var marker = L.marker([10.094656301015542, -84.37315463422155]).addTo(map)
+      .bindPopup('Reciclaje Arrieta')
+      .openPopup();
+
+    var marker = L.marker([9.884685364150329, -83.94159793242802]).addTo(map)
+      .bindPopup('Recicladora Hernández Cartín')
+      .openPopup();
+  </script>
 
 </body>
 
